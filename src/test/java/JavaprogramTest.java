cb3192464951684f06e0b30e081a47fe4b67c189
sangesh <sangesh3113@gmail.com>
2026-03-12 12:27:28 +0530

    Added regression test

---
+++ src/test/java/JavaprogramTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JavaprogramTest {
+
+    @Test
+    public void testAddition() {
+        int result = 10 + 5;
+        assertEquals(15, result);
+    }
+}
